8f77fccc8960d120fc2995704e4a747040c587ee
soundarya788 <soundaryaa040@gmail.com>
2023-05-04 10:02:48 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-)
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+
+mongoose.connect("mongodb+srv://soundaryasaravanan68:soundaryasaravanan@cluster0.jx3ojuz.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("DB connected")
+})
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
+//Routes
+app.post("/login", (req, res)=> {
+    const { email, password} = req.body
+    User.findOne({ email: email}, (err, user) => {
+        if(user){
+            if(password === user.password ) {
+                res.send({message: "Login Successfull", user: user})
+            } else {
+                res.send({ message: "Password didn't match"})
+            }
+        } else {
+            res.send({message: "User not registered"})
+        }
+    })
+}) 
+
+app.post("/register", (req, res)=> {
+    const { name, email, password} = req.body
+    User.findOne({email: email}, (err, user) => {
+        if(user){
+            res.send({message: "User already registerd"})
+        } else {
+            const user = new User({
+                name,
+                email,
+                password
+            })
+            user.save(err => {
+                if(err) {
+                    res.send(err)
+                } else {
+                    res.send( { message: "Successfully Registered" })
+                }
+            })
+        }
+    })
+    
+}) 
+
+app.listen(9002,() => {
+    console.log("BE started at port 9002")
+})
